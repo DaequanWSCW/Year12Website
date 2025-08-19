b3ba0dc33cd5b6a18a39d85b97c59c9dccede60e
DaequanWSCW <sud@wsc.school.nz>
2025-08-19 15:29:16 +1200

    QUIZ COMPLETED SPRINT 3 COMPLETED

--- script.js
+++ script.js
@@ -122,14 +122,45 @@
         }
     }
 
-function selectAnswer(){
+function selectAnswer(e){
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
-    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
 
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You got a score of ${score} out of ${questions.length}! Good Job!`;
+    nextButton.innerHTML = "Restart";
+    nextButton.style.display = "block";
+}               
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length){    
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
     startQuiz();
